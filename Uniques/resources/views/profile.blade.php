3be6b93514420224eb774f3055943de2648a8d34
AlexisGBarrientos <alexisgbarrientos@gmail.com>
2020-03-07 10:58:56 -0300

    views profile

--- Uniques/resources/views/profile.blade.php
+++ Uniques/resources/views/profile.blade.php
@@ -5,21 +5,24 @@
 @section('cssLink', '/css/bootstrap.min.css')
 
 @section('mainContent')
-  <div class="container" style="align-items: center;">
+  <div class="container" style="align-items: start;">
       <div class="row justify-content-center">
           <div class="col-xs-12 col-sm-8 col-md-6">
             <div class="card">
-              <div class="col-sm-6 col-md-6">
-                <img src="/storage/{{ Auth::user()->avatar }}" class="card-img-top" alt="{{ Auth::user()->name }}" class="img-rounded img-responsive">
+              <div class="card-body">
+                <img src="/storage/avatars/{{ Auth::user()->avatar }}" class="card-img-top" alt="{{ Auth::user()->name }}" class="img-rounded img-responsive">
               </div>
 
-                <h2>Welcome {{ Auth::user()->name }} {{ Auth::user()->surname }}</h2>
-                <!-- {{ Auth::user() }} -->
-                <hr>
-                  <p><i class="fas fa-envelope"></i> {{ Auth::user()->email }}</p>
-                  <p><i class="fab fa-facebook"></i> {{ Auth::user()->name }} {{ Auth::user()->surname }}</p>
-                <hr>
-                
+                <div class="card-body">
+                  <h2>Welcome {{ Auth::user()->name }} {{ Auth::user()->surname }}</h2>
+                  <!-- {{ Auth::user() }} -->
+                  <hr>
+                    <p><i class="fas fa-envelope"></i> {{ Auth::user()->email }}</p>
+                    <p><i class="fab fa-facebook"></i> {{ Auth::user()->name }} {{ Auth::user()->surname }}</p>
+                  <hr>
+                </div>
+
+
                 <div class="btn-group row mb-0">
                   <div class="col-md-6 offset-md-3 text-center">
                     <form class="" action="/user-edit/{{ Auth::user()->id}}/edit" method="get">
